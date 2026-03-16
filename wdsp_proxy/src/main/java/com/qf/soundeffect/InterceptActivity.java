e7c91d7be77b521201d1e129b0a2f6ad636980ee
khiivl <73498201+khiivl@users.noreply.github.com>
2026-03-17 00:30:34 +0100

    added wDSP_proxy to the project

---
+++ wdsp_proxy/src/main/java/com/qf/soundeffect/InterceptActivity.java
@@ -0,0 +1,20 @@
+package com.qf.soundeffect;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class InterceptActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Launch your real app
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.radiorubka.wdsp");
+        if (intent != null) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent);
+        }
+        finish();
+        overridePendingTransition(0, 0);
+    }
+}
